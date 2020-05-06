146d5f77540128df0658dcf84ae9d447651d3766
Bridget Asser <bridgetasser@gmail.com>
2020-05-05 22:49:19 -0400

    created CustomerType, can fetch hardcoded customers via Graphql IDE

--- schema.js
+++ schema.js
@@ -7,6 +7,16 @@ const {
     GraphQLNonNull,
 } = require("graphql");
 
+//hardcoded data, will connect db
+
+const customers = [
+    { id: "1", name: "Bridget Asser", email: "bridget@yahoo.com", age: 97 },
+    { id: "2", name: "Bridgete Doe", email: "doe@yahoo.com", age: 35 },
+    { id: "3", name: "Andrew Yang", email: "yang@aol.com", age: 50 },
+    { id: "4", name: "Steve Smith", email: "steve@yahoo.com", age: 21 },
+    { id: "5", name: "Dave Man", email: "dave@ygmail.com", age: 49 },
+];
+
 const CustomerType = new GraphQLObjectType({
     name: "Customer",
     fields: () => ({
@@ -19,9 +29,30 @@ const CustomerType = new GraphQLObjectType({
 
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
-    customer: {
-        type: CustomerType,
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: { type: GraphQLString },
+            },
+            resolve(parentValue, args) {
+                //while hardcoded, will make JSON req
+                for (let i = 0; i < customers.length; i++) {
+                    if (customers[i].id == args.id) {
+                        return customers[i];
+                    }
+                }
+            },
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args) {
+                return customers;
+            },
+        },
     },
 });
 
-module.exports = new GraphQLSchema({});
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+});
